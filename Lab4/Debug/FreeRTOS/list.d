ae3478c17773edd0491b13319862747fb3665133
jhoutjyt <47512925+jhoutjyt@users.noreply.github.com>
2024-06-18 16:43:39 +0800

    Add files via upload

---
+++ Lab4/Debug/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/projdefs.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/portable.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/list.h
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/FreeRTOS.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/projdefs.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/portable.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/user/STM32CubeIDE/workspace_1.12.0/Lab4_N26094257/FreeRTOS/include/list.h:
